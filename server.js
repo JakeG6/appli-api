0fa9b727c4f07d49ee9e2bc37ab03cd25b113074
Jacob <jakeguss@gmail.com>
2019-04-15 22:48:54 -0600

    specified access-control-allow-origin

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = require('./db.js')
 //app.use(cors())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", 'https://appli-front.herokuapp.com');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
