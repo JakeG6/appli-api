af2185166be1bdd2bdd2da3d7412cb6b5a6adf74
Jacob <jakeguss@gmail.com>
2019-04-17 18:12:36 -0600

    wildcard added

--- headers.js
+++ headers.js
@@ -1,6 +1,6 @@
 
 module.exports = (req, res, next) => {
-    res.header('access-control-allow-origin', 'https://appli-front.herokuapp.com')
+    res.header('access-control-allow-origin', '*')
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS')
     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()
