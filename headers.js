f6845b6fb0b25d1529ee212ac2bed3796667fe3a
Jacob <jakeguss@gmail.com>
2019-04-17 18:16:48 -0600

    new headers

--- headers.js
+++ headers.js
@@ -1,7 +1,8 @@
 
 module.exports = (req, res, next) => {
-    res.header('access-control-allow-origin', '*')
-    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next()
 }
